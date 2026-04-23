2192ef137d5537e60f30f816153fb5bb24e0095c
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-23 23:44:49 +0530

    added files

---
+++ Collection/ArrayList/program2/Employee.java
@@ -0,0 +1,20 @@
+package Advance_Java.Collection.ArrayList.program2;
+
+public class Employee {
+   public int id;
+   public String name;
+   public double salary;
+   
+  public Employee(int id,String name,double salary){
+	   this.id=id;
+	   this.name=name;
+	   this.salary=salary;
+   }
+
+  @Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+}
+  
+  
+}
